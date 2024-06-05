a9750d44b13f37913662cc677cfcef4594814223
JakoNito <tss1799@autuni.ac.nz>
2024-06-05 20:28:43 +1200

    Organize Code into sub folders
    Organize code into subfolders according to MVC:
    - Model
    - Controller
    - View

---
+++ Quiz_Learning_App/src/QuizApp_CLI/Controller/MultiChoiceMode.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package QuizApp_CLI.Controller;
+
+/**
+ *
+ * @author jakoi
+ */
+import QuizApp_CLI.Model.Question;
+import QuizApp_CLI.View.QuestionDisplayMode;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class MultiChoiceMode extends QuestionDisplayMode {
+    private List<String> choices;
+    private Scanner scanner;
+
+    public MultiChoiceMode(List<String> choices, Scanner scanner) {
+        this.choices = choices;
+        this.scanner = scanner;
+    }
+
+    @Override
+    public void displayQuestion(Question question) {
+        System.out.println(question.getQuestion());
+        List<String> allChoices = new ArrayList<>(choices); // Start with common choices
+        List<String> questionChoices = question.getChoices(); // Get question-specific choices
+        for (String choice : questionChoices) {
+            if (!allChoices.contains(choice)) { // Add only if not already present
+                allChoices.add(choice);
+            }
+        }
+        if (!allChoices.contains(question.getCorrectAnswer())) { // Ensure correct answer is included
+            allChoices.add(question.getCorrectAnswer());
+        }
+        Collections.shuffle(allChoices);
+        for (int i = 0; i < allChoices.size(); i++) {
+            System.out.println((char) ('a' + i) + ". " + allChoices.get(i));
+        }
+    }
+}
